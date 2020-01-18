27f1e519cd36b7a871b1a95bd349ea173487e7fe
yohei0125 <yohei0125@weseek.co.jp>
2020-01-19 00:05:03 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { createStore, applyMiddleware } from 'redux'
 // 作成したSTOREを全コンポーネントに渡すための機能を持つprovider
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import "./index.css";
 // reducer
 import reducer from './reducers'
 // component専用のディレクトリがあると見通しがよくアンル
 import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
 import registerServiceWorker from "./registerServiceWorker";
 // ここで作られるreducerはapplication内で唯一の物になる
 // application内部の全てstateはここに集約されている
@@ -18,9 +20,14 @@ import registerServiceWorker from "./registerServiceWorker";
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <EventsIndex />
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 // Combine all reducers in the application into one reducer
 // import package to combine 
 import { combineReducers } from "redux"
+import { reducer as form } from 'redux-form'
 // this has the state to use in the application
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
