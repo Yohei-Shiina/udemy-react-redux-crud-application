c1935f16873ee3188a301f18c3cff6cacbda47cf
yohei0125 <yohei0125@weseek.co.jp>
2020-01-18 16:40:00 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from 'redux'
+// 作成したSTOREを全コンポーネントに渡すための機能を持つprovider
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+// reducer
+import reducer from './reducers'
+// component専用のディレクトリがあると見通しがよくアンル
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+// ここで作られるreducerはapplication内で唯一の物になる
+// application内部の全てstateはここに集約されている
+// このStoreが全階層のどのcomponentからでも参照できるようにする
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 registerServiceWorker();
